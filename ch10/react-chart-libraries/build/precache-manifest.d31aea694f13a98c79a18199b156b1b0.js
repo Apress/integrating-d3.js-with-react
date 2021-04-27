30c2789f16941679c133c509372efcc8ffce9583
MarkP88 <markpowers@apress.com>
2021-04-27 13:28:55 -0400

    Add files via upload

---
+++ ch10/react-chart-libraries/build/precache-manifest.d31aea694f13a98c79a18199b156b1b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad10d8304bbe01c90524f246b54eeca7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b124f6135c96e73a5f04",
+    "url": "/static/css/2.5a2f7233.chunk.css"
+  },
+  {
+    "revision": "4f451fc87b827b8a9fc2",
+    "url": "/static/css/main.7a4fb01d.chunk.css"
+  },
+  {
+    "revision": "b124f6135c96e73a5f04",
+    "url": "/static/js/2.d6ebf2ae.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/static/js/2.d6ebf2ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f451fc87b827b8a9fc2",
+    "url": "/static/js/main.b4acb14e.chunk.js"
+  },
+  {
+    "revision": "d03d62c1536c12c88c90",
+    "url": "/static/js/runtime-main.e2745115.js"
+  }
+]);
