babf30441c0a4a24d06b5df7e7134b0b2dc854f4
MarkP88 <markpowers@apress.com>
2021-04-27 13:13:54 -0400

    Add files via upload

---
+++ ch01/react-d3-hello-world/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.scss'
+import HelloD3 from './components/HelloD3/HelloD3'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <HelloD3 />
+      </header>
+    </div>
+  )
+}
+
+export default App
